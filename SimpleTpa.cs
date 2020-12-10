4870243cceb55c3286a889877e9aa379b8e15581
Senior S <maximatador23@gmail.com>
2020-12-10 18:43:04 -0300

    First release

---
+++ SimpleTpa.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Cysharp.Threading.Tasks;
+using OpenMod.Unturned.Plugins;
+using OpenMod.API.Plugins;
+using SDG.Unturned;
+using System.Linq;
+using System.Collections.Generic;
+using OpenMod.Unturned.Users;
+using OpenMod.API.Users;
+using OpenMod.Core.Helpers;
+using OpenMod.Core.Users;
+using System.Threading.Tasks;
+
+[assembly: PluginMetadata("SS.SimpleTpa", DisplayName = "SimpleTpa")]
+namespace SimpleTpa
+{
+    public class SimpleTpa : OpenModUnturnedPlugin
+    {
+        private readonly IConfiguration m_Configuration;
+        private readonly IStringLocalizer m_StringLocalizer;
+        private readonly IUserManager m_UserManager;
+        private readonly ILogger<SimpleTpa> m_Logger;
+
+        public SimpleTpa(
+            IConfiguration configuration, 
+            IStringLocalizer stringLocalizer,
+            ILogger<SimpleTpa> logger, 
+            IUserManager userManager,
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_Configuration = configuration;
+            m_StringLocalizer = stringLocalizer;
+            m_UserManager = userManager;
+            m_Logger = logger;
+        }
+
+        public async UniTask TelportPlayer(UnturnedUser target, UnturnedUser player) // player TO target
+        {
+            int interval = m_Configuration.GetSection("plugin_configuration:tpainterval").Get<int>() - 3;
+            await Task.Delay(TimeSpan.FromSeconds(interval));
+            await player.PrintMessageAsync("3...");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            await player.PrintMessageAsync("2...");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            await player.PrintMessageAsync("1...");
+            await player.PrintMessageAsync(m_StringLocalizer["plugin_translation:tpa_now"]);
+            await UniTask.SwitchToMainThread();
+
+            if (!target.Player.IsAlive)
+            {
+                await player.PrintMessageAsync(m_StringLocalizer["plugin_translation:tpa_error_target_dead"]);
+            }
+            else if (!player.Player.IsAlive)
+            {
+                await player.PrintMessageAsync(m_StringLocalizer["plugin_translation:tpa_error_user_dead"]);
+            }
+            else
+            {
+                player.Player.Player.teleportToLocationUnsafe(target.Player.Player.transform.position, target.Player.Player.look.yaw);
+            }
+        }
+
+        protected override async UniTask OnLoadAsync()
+        {
+            m_Logger.LogInformation("Plugin loaded correctly!");
+            m_Logger.LogInformation("More plugins: www.dvtserver.xyz");
+
+            SteamChannel.onTriggerSend += OnTriggerSend;
+        }
+
+        // tellLean - Arguments: Default: 1 - Derecha/Right: 0 - Izquierda/Left: 2
+        private void OnTriggerSend(SteamPlayer s, string name, ESteamCall mode, ESteamPacket type, object[] R)
+        {
+            if (name == "tellLean")
+            {
+                AsyncHelper.RunSync(async () => {
+                    UnturnedUser user = (UnturnedUser)await m_UserManager.FindUserAsync(KnownActorTypes.Player, s.playerID.steamID.ToString(), UserSearchMode.FindById);
+                    if (PendingTeleports.Any(k => k.Value == user))
+                    {
+                        if (R[0].ToString() == "2")
+                        {
+                            var toRemove = PendingTeleports.Where(k => k.Value == user).First();
+                            var sender = toRemove.Key;
+                            PendingTeleports.Remove(toRemove.Key);
+                            await sender.PrintMessageAsync(m_StringLocalizer["plugin_translation:tpa_cancelled"]);
+                        }
+                        else if (R[0].ToString() == "0")
+                        {
+                            var toRemove = PendingTeleports.Where(k => k.Value == user).First();
+                            var sender = toRemove.Key;
+                            PendingTeleports.Remove(toRemove.Key);
+                            await sender.PrintMessageAsync(m_StringLocalizer["plugin_translation:tpa_accepted", new { USER = user.DisplayName, INTERVAL = m_Configuration.GetSection("plugin_configuration:tpainterval").Get<int>() }]);
+                            AsyncHelper.Schedule("Start Teleport", () => TelportPlayer(user, sender).AsTask());
+                        }
+                    }
+                });
+            }
+        }
+
+        protected override async UniTask OnUnloadAsync()
+        {
+            // await UniTask.SwitchToMainThread();
+            m_Logger.LogInformation("Plugin unloaded!");
+            m_Logger.LogInformation("More plugins: www.dvtserver.xyz");
+        }
+
+        internal static Dictionary<UnturnedUser, UnturnedUser> PendingTeleports = new Dictionary<UnturnedUser, UnturnedUser>();
+        internal static Dictionary<UnturnedUser, DateTime> TpaCooldown = new Dictionary<UnturnedUser, DateTime>();
+    }
+}
---
+++ TpaCommand.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using OpenMod.API.Commands;
+using OpenMod.API.Users;
+using OpenMod.Core.Commands;
+using OpenMod.Core.Users;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using System;
+using System.Threading.Tasks;
+using Command = OpenMod.Core.Commands.Command;
+
+namespace SimpleTpa
+{
+    [Command("teleportask")]
+    [CommandAlias("tpa")]
+    [CommandAlias("tpask")]
+    [CommandSyntax("<player>")]
+    [CommandDescription("Ask teleport to a player.")]
+    public class TpaCommand : Command
+    {
+        private readonly IUserManager m_UserManager;
+        private readonly IStringLocalizer m_StringLocalizer;
+
+        public TpaCommand(IServiceProvider serviceProvider, IUserManager userManager, IStringLocalizer stringLocalizer) : base(serviceProvider)
+        {
+            m_UserManager = userManager;
+            m_StringLocalizer = stringLocalizer;
+        } 
+
+        protected override async Task OnExecuteAsync()
+        {
+            UnturnedUser user = (UnturnedUser)Context.Actor;
+            if (Context.Parameters.Count != 1)
+            {
+                throw new CommandWrongUsageException("Error! Correct usage: /tpa <player>");
+            }
+
+            string tname = await Context.Parameters.GetAsync<string>(0);
+
+            Player target = PlayerTool.getPlayer(tname); 
+
+            if (target == null)
+            {
+                throw new UserFriendlyException(m_StringLocalizer["plugin_translation:tpa_error_player_not_found"]);
+            }
+            else if (target == user.Player.Player)
+            {
+                throw new UserFriendlyException(m_StringLocalizer["plugin_translation:tpa_error_yourself"]);
+            }
+            else
+            {
+                UnturnedUser targetUser = (UnturnedUser)await m_UserManager.FindUserAsync(KnownActorTypes.Player, target.channel.owner.playerID.steamID.ToString(), UserSearchMode.FindById);
+
+                await targetUser.PrintMessageAsync(m_StringLocalizer["plugin_translation:tpa_received", new { USER = user.DisplayName }]);
+                SimpleTpa.PendingTeleports.Add(user, targetUser);
+            }
+        }
+    }
+}
